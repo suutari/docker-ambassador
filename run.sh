706d1e53f64de80b0dfcd34f8c2f26600d34edd3
Tuomas Suutari <tuomas.suutari@gmail.com>
2014-10-25 21:10:20 +0300

    run.sh: Reset IFS to space

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ trap "[ -n \"\$socat_pids\" ] && kill \$socat_pids" EXIT
 for rule in `env | sed -n "\%$search%{s%$search%\1,\2:\3%;p}"`; do
     IFS=,
     set -- $rule
+    IFS=" "
     src=$1
     dst=$2
     socat -ls TCP4-LISTEN:$src,fork,reuseaddr TCP4:$dst &
